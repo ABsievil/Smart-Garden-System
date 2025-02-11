c80484706035dd6a33e5f522ccc2d6718e0b2632
ABsievil <nccuong317qb@gmail.com>
2025-02-11 23:13:14 +0700

    fix: typo in name test class

--- src/test/java/hcmut/smart_garden_system/SmartGardenSystemApplicationTests.java
+++ src/test/java/hcmut/smart_garden_system/SmartGardenSystemApplicationTests.java
@@ -1,10 +1,10 @@
-package hcmut.smart_gardent_system;
+package hcmut.smart_garden_system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SmartGardentSystemApplicationTests {
+class SmartGardenSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
