89ccf9e0865bc21822f03aa386d2274f94e7ccf1
ABsievil <nccuong317qb@gmail.com>
2025-02-27 16:35:34 +0700

    docs(BE): add login DTO

---
+++ backend-springboot/src/main/java/hcmut/smart_garden_system/DTOs/LoginDTO.java
@@ -0,0 +1,15 @@
+package hcmut.smart_garden_system.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDTO {
+    private String username;
+    private String password;
+}
