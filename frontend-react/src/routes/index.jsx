e50279b87738896adafeff4a60ecf7f84cdc9245
AnhAnh1212 <kavilnina13@gmail.com>
2025-03-03 11:39:38 +0700

    fix(FE-Admin Folder):rename admin folder

--- frontend-react/src/routes/index.jsx
+++ frontend-react/src/routes/index.jsx
@@ -1,11 +1,11 @@
 import { Navigate } from 'react-router-dom';
 import Layout1 from '../Components/Layouts/Layout1/Layout1';
 import Layout2 from "../Components/Layouts/Layout2/Layout2";
-import Dashboard from "../Pages/Admin /Dashboard/Dashboard";
-import DeviceManager from "../Pages/Admin /DeviceManager/DeviceManager";
-import Scheduler from "../Pages/Admin /Scheduler/Scheduler";
-import StaffManager from "../Pages/Admin /StaffManager/StaffManager";
-import TreeManager from "../Pages/Admin /TreeManager/TreeManager";
+import Dashboard from "../Pages/Admin/Dashboard/Dashboard";
+import DeviceManager from "../Pages/Admin/DeviceManager/DeviceManager";
+import Scheduler from "../Pages/Admin/Scheduler/Scheduler";
+import StaffManager from "../Pages/Admin/StaffManager/StaffManager";
+import TreeManager from "../Pages/Admin/TreeManager/TreeManager";
 import ForgetPassword from "../Pages/Login/ForgetPassword/ForgetPassword";
 import SignIn from "../Pages/Login/Signin/Login";
 import ControlDevice from "../Pages/Staff/ControlDevice/ControlDevice";
