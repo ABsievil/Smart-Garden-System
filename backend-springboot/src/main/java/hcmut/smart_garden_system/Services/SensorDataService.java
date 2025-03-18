202dea03e8d6973eb7035ef0503979f219b31c41
ABsievil <nccuong317qb@gmail.com>
2025-03-18 14:32:34 +0700

    docs(BE): add code save sensor data to db

--- backend-springboot/src/main/java/hcmut/smart_garden_system/Services/SensorDataService.java
+++ backend-springboot/src/main/java/hcmut/smart_garden_system/Services/SensorDataService.java
@@ -10,10 +10,13 @@ import hcmut.smart_garden_system.Models.SensorData;
 public class SensorDataService {
     private static SensorData latestData;
     private static final ObjectMapper mapper = new ObjectMapper();
-    
+    private static SensorDataService sensorDataService;
+
     public static void updateLatestData(String jsonData) {
         try {
             latestData = mapper.readValue(jsonData, SensorData.class);
+            sensorDataService.PROC_saveRecord(latestData);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
