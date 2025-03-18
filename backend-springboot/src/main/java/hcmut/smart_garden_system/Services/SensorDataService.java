ae2f091fb7668f9e21ff7eac68804d106925145a
ABsievil <nccuong317qb@gmail.com>
2025-03-18 14:52:37 +0700

    fix(BE): error code in SensorDataService class

--- backend-springboot/src/main/java/hcmut/smart_garden_system/Services/SensorDataService.java
+++ backend-springboot/src/main/java/hcmut/smart_garden_system/Services/SensorDataService.java
@@ -5,17 +5,17 @@ import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import hcmut.smart_garden_system.Models.SensorData;
-
+import hcmut.smart_garden_system.Services.RestfulAPI.RecordService;
 @Service
 public class SensorDataService {
     private static SensorData latestData;
     private static final ObjectMapper mapper = new ObjectMapper();
-    private static SensorDataService sensorDataService;
+    private static RecordService recordService;
 
     public static void updateLatestData(String jsonData) {
         try {
             latestData = mapper.readValue(jsonData, SensorData.class);
-            sensorDataService.PROC_saveRecord(latestData);
+            recordService.PROC_saveRecord(latestData);
 
         } catch (Exception e) {
             e.printStackTrace();
