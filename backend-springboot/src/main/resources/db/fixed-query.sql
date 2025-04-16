ff9b90e624e383335a449464061d7f7f018419ca
ABsievil <nccuong317qb@gmail.com>
2025-04-16 14:07:00 +0700

    docs(db): add some proc and func for email service

--- backend-springboot/src/main/resources/db/fixed-query.sql
+++ backend-springboot/src/main/resources/db/fixed-query.sql
@@ -68,3 +68,52 @@ BEGIN
     WHERE nameofdevices = device_name_input;
 END;
 $$;
+
+
+--28--Lưu otp
+CREATE OR REPLACE PROCEDURE add_otp_by_email(email_input VARCHAR, otp_code_input VARCHAR)
+LANGUAGE plpgsql AS $$ 
+DECLARE
+    user_exists BOOLEAN;
+BEGIN
+    -- Kiểm tra xem email có tồn tại trong bảng users không
+    SELECT EXISTS (SELECT 1 FROM users WHERE email = email_input) INTO user_exists;
+
+    -- Nếu email không tồn tại, báo lỗi
+    IF NOT user_exists THEN
+        RAISE EXCEPTION 'Email not found: %', email_input;
+    END IF;
+
+    -- Nếu email tồn tại, thêm OTP
+    INSERT INTO OTP (email, otp_code)
+    VALUES (email_input, otp_code_input);
+END;
+$$;
+--CALL add_otp_by_email('tinhquach@hcmut.edu.vn','1234m5')
+
+--29--Xóa otp
+CREATE OR REPLACE PROCEDURE delete_otp_by_email(email_input VARCHAR)
+LANGUAGE plpgsql AS $$
+BEGIN
+    DELETE FROM OTP
+	WHERE email = email_input;
+END;
+$$;
+-- CALL delete_otp_by_email('vumakhmtk22@hcmut.edu.vn')
+
+--30--Lấy thông tin otp
+CREATE OR REPLACE FUNCTION get_otp_by_email(email_input VARCHAR)
+RETURNS JSON AS $$
+DECLARE
+    result JSON;
+BEGIN
+    SELECT json_build_object(
+			'otp_code', otp_code
+       		)
+    INTO result
+    FROM OTP
+	WHERE email = email_input;
+    
+    RETURN result;
+END;
+$$ LANGUAGE plpgsql;
